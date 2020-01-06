3b91307188606c2cc99b0bc447c2ffa01a46aee5
John Cox <johncox@gmail.com>
2020-01-06 11:11:01 -0700

    adds a note for other developers

--- lib/each_with_progress.rb
+++ lib/each_with_progress.rb
@@ -1,5 +1,6 @@
 require_relative 'core_extensions/enumerable_progress'
 
+# There's probably a better way to do this. By all means submit a PR if you know of one...
 ObjectSpace.each_object(Class).select { |c| c.included_modules.include? Enumerable }.each do |klazz|
   klazz.include CoreExtensions::EnumerableProgress
 end
