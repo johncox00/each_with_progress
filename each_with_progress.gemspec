4c43ca1d4634986e18d405cdf0b94a151fbf1d7a
John Cox <johncox@gmail.com>
2020-01-07 11:44:18 -0700

    fixes frozen error

--- each_with_progress.gemspec
+++ each_with_progress.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'each_with_progress'
-  s.version     = '0.0.2'
+  s.version     = '0.0.3'
   s.date        = '2020-01-06'
   s.summary     = "Adds progress bar output to Enumerable each functions."
   s.description = "Adds progress bar output to Enumerable each functions."
--- lib/each_with_progress.rb
+++ lib/each_with_progress.rb
@@ -2,5 +2,9 @@ require_relative 'core_extensions/enumerable_progress'
 
 # There's probably a better way to do this. By all means submit a PR if you know of one...
 ObjectSpace.each_object(Class).select { |c| c.included_modules.include? Enumerable }.each do |klazz|
-  klazz.include CoreExtensions::EnumerableProgress
+  begin
+    klazz.include CoreExtensions::EnumerableProgress
+  rescue FrozenError => e
+    puts "Cannot use each_with_progress in #{klazz}. Skipping..."
+  end
 end
